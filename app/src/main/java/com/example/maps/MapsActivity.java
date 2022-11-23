74f74bdbad556d3d1975aee17db53999332f8ab6
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-23 11:39:22 +0300

    add a method to initialize the map layout.

--- app/src/main/java/com/example/maps/MapsActivity.java
+++ app/src/main/java/com/example/maps/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.maps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -9,16 +10,26 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MapsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String COURSE_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final String FINE_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int REQUEST_CODE = 1111;
     private Boolean isOk = false;
+    GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+    }
+
+    public void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     public void getLocations(){
@@ -30,5 +41,30 @@ public class MapsActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this,permissions, REQUEST_CODE );
             }
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        isOk = false;
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            isOk = false;
+                            return;
+                        }
+                    }
+                    isOk = true;
+                    //Initialize the map since all permissions are granted
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
     }
 }
